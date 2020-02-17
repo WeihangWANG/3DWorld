f993ac34c430a969739f91afeda7757ad766f18a
WWH_SJTU <wangweihang_china@163.com>
2020-02-17 19:05:11 +0800

    CODE structure of 3D world

--- mkdir.py
+++ mkdir.py
@@ -4,6 +4,11 @@ for i in range(2,8):
     if not os.path.exists("ch%s"%i):
         os.mkdir("ch%s"%i)
     for j in range(1,len[i]):
-        os.mkdir("ch%s/%s.%s"%(i,i,j))
+        if not os.path.exists("ch%s" % i):
+            os.mkdir("ch%s/%s.%s"%(i,i,j))
+        else:
+            f = open("ch%s/%s.%s/none.txt"%(i,i,j),'w')
+            f.write("none")
+            f.close()
 
 
