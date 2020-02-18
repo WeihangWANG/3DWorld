f41055f4331464628c9610c013c8347e0ee128ff
贾佳璐 <420509576@qq.com>
2020-02-18 11:55:26 +0800

    Update readbinfile.py

--- ch3/3.6/readbinfile.py
+++ ch3/3.6/readbinfile.py
@@ -11,7 +11,7 @@ savefile = open("./data/savetest.bin", 'wb') # 'wb'写入模式
 IMG_HGT = 240
 IMG_WID = 320
 IMG_SIZE = IMG_HGT*IMG_WID
-Importimport numpy as np
+import numpy as np
 for i in range(MAX_CNT):
 # 此时读入的frame_test是一个一维数组，深度图每行的首尾连接
     frame_test = np.fromfile(readfile, dtype=np.uint16, count=IMG_ SIZE)
